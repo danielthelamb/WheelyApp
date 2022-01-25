9e93203464649ca8eaf6a50f403904c894e0f8e7
Daniel Lam <pickypickleapp@gmail.com>
2022-01-25 17:41:31 +1300

    Added some folders and set up the basic stucture

---
+++ WheelyApp/WheelyApp/Model/Option.swift
@@ -0,0 +1,16 @@
+//
+//  Option.swift
+//  WheelyApp
+//
+//  Created by Daniel Lam on 25/01/22.
+//
+
+import Foundation
+
+struct Option {
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
